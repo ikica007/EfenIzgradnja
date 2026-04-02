a431da8150e7e597bc285dd30479cc609831f168
ikica007 <108173486+ikica007@users.noreply.github.com>
2026-04-02 14:44:23 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import ContactMap from './ContactMap';
 import { useContent } from '../hooks/useContent';
 
 export default function Footer() {
-    const address = 'Bulevar Pera Ćetkovića 115, Podgorica, Crna Gora';
+    const address = 'Bulevar Crnogorskih Junaka 100, Cetinje, Crna Gora';
     const phone = '069 170 627';
     const email = 'efencolor@gmail.com';
 
